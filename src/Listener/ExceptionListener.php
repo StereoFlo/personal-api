a0346d6d23f80ab033f58798adbe3319cf58efdd
Evgeniy <evgeniy@stereoflo.ru>
2018-05-08 11:50:34 +0300

    fix

--- src/Listener/ExceptionListener.php
+++ src/Listener/ExceptionListener.php
@@ -4,6 +4,7 @@ namespace App\Listener;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 /**
  * Class ExceptionListener
@@ -19,8 +20,8 @@ class ExceptionListener
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
-        if ($exception) {
-            $response = JsonResponse::create(['success' => false, 'message' => $exception->getMessage()], $exception->getStatusCode());
+        if ($exception instanceof HttpExceptionInterface) {
+            $response = JsonResponse::create(['success' => false, 'message' => $exception->getMessage()], $exception->getStatusCode(), $exception->getHeaders());
             $event->setResponse($response);
         }
     }
