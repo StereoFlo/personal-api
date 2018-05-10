8b7f0fca1273e1c5bddbf272b9d3ad5f413e197b
Evgeniy <evgeniy@stereoflo.ru>
2018-05-10 13:15:59 +0300

    login

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Commands\UserRegisterCommand;
+use App\Repository\UserInterface;
 use League\Tactician\CommandBus;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,14 +19,18 @@ class AuthController
      */
     private $bus;
 
+    private $userRepository;
+
     /**
      * AuthController constructor.
      *
-     * @param CommandBus $bus
+     * @param CommandBus    $bus
+     * @param UserInterface $userRepository
      */
-    public function __construct(CommandBus $bus)
+    public function __construct(CommandBus $bus, UserInterface $userRepository)
     {
         $this->bus = $bus;
+        $this->userRepository = $userRepository;
     }
 
     /**
@@ -42,5 +47,35 @@ class AuthController
         $this->bus->handle(new UserRegisterCommand($username, $email, $password));
 
         return new JsonResponse(['success' => true, 'message' => 'success registred']);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $email    = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        if (empty($email) || empty($password)) {
+            throw new \Exception('form.input.empty');
+        }
+
+        $user = $this->userRepository->getByEmail($email);
+
+        if (!$user) {
+            throw new \Exception('login.not.found');
+        }
+
+        if (!\password_verify($password, $user->getPassword())) {
+            throw new \Exception('invalid.password');
+        }
+
+        $this->userRepository->save($user->setUpdatedAt());
+
+        return new JsonResponse(['success' => true, 'message' => 'success logged in', 'data' => ['token' => $user->getApiToken()->getKey()]]);
     }
 }
