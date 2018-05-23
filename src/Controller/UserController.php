bcc65089570b0f3e9aafd5f6025f3d5f3458669a
Evgeniy <evgeniy@stereoflo.ru>
2018-05-23 12:16:49 +0300

    get user for check auth

---
+++ src/Controller/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\User\UserInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ */
+class UserController
+{
+    /**
+     * @var User|null
+     */
+    private $user;
+
+    /**
+     * @var UserInterface
+     */
+    private $userRepo;
+
+    /**
+     * @var AbstractController
+     */
+    private $controller;
+
+    /**
+     * UserController constructor.
+     *
+     * @param User|null          $user
+     * @param UserInterface      $userRepo
+     * @param AbstractController $controller
+     */
+    public function __construct(?User $user, UserInterface $userRepo, AbstractController $controller)
+    {
+        $this->user       = $user;
+        $this->userRepo   = $userRepo;
+        $this->controller = $controller;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function getUser(): JsonResponse
+    {
+        if (empty($this->user)) {
+            return $this->controller->errorJson('user.empty', 401);
+        }
+        return $this->controller->json($this->user);
+    }
+}
